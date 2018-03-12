0d3b12c7ff064e4cb246e73a52712c6602586e2d
Steven <steven.s.gutier@gmail.com>
2018-03-12 01:49:22 -0700

    Jsoup included, project ready to begin

--- src/menuMain.java
+++ src/menuMain.java
@@ -1,9 +1,15 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class menuMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Document porterMenu = Jsoup.connect("http://nutrition.sa.ucsc.edu/nutframe.asp?locationNum=25&locationName=Porter").get();
+		log(porterMenu.title());
+		
 	}
 
 }
